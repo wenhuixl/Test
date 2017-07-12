08ec3b713441ce5c4d74f90b6fb1a4f144f2e0f6
wenhuixl <2786076689@qq.com>
2017-07-12 15:02:49 +0800

    Create demo-001.js
    理解node.js赋值

---
+++ demo-001.js
@@ -0,0 +1,21 @@
+/**
+ * Created by wenhui on 2017/7/12.
+ */
+
+'use strict'
+
+var a = {name: 'wenhui'};
+var b = a;
+
+console.log('a:', a)
+console.log('b:', b)
+
+b.name = 'xiaojun';
+
+console.log('a:', a)
+console.log('b:', b)
+
+var b = {name: 'xiaosi'};
+
+console.log('a:', a)
+console.log('b:', b)
